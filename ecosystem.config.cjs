09e48a5f6c23362b19892d66eac05ea77e9d7cb2
tube10081008-stack <tube10081008@gmail.com>
2026-01-06 00:28:17 +0900

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   apps: [{
-    name: 'daily-condition-letter',
+    name: 'daily-letter',
     script: './dist/index.js',
     instances: 1,
     autorestart: true,
@@ -9,10 +9,6 @@ module.exports = {
     env: {
       NODE_ENV: 'production',
       PORT: 3000
-    },
-    error_file: './logs/error.log',
-    out_file: './logs/out.log',
-    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-    merge_logs: true
+    }
   }]
 };
